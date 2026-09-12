cdf4b3bc8efe7a469c85c7940a13486f65d58dba
Vishal Meena <vishalmeena6898@gmail.com>
2026-09-12 21:53:22 +0530

    Enable Jetpack Compose in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,14 @@ android {
         versionName = "1.0"
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -32,8 +40,15 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+
+    implementation("androidx.compose.material3:material3:1.3.1")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
 }
